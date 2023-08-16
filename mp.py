2cf77af437c5f9e22c9d6278be963146bed3f1e6
techteston <126379595+techteston@users.noreply.github.com>
2023-08-16 19:15:10 +0530

    Update mp.py

--- mp.py
+++ mp.py
@@ -100,8 +100,8 @@ with st.container():
         df
 
 df = pd.DataFrame({'col1': [1, 2, 3], 'col2': [4, 5, 6]})
- grid_return = AgGrid(df, editable=True)
- new_df = grid_return['data']
+grid_return = AgGrid(df, editable=True)
+new_df = grid_return['data']
 
 # st.write("---")
 
